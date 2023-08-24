a3b60f8b3fc22437427a088f0c4a9267d1e18a86
225junior <225juniorbrindou@gmail.com>
2023-08-24 12:10:15 +0000

    feat: Add InterfaceTo energizer the code

---
+++ src/app/Models/PersonInterface.ts
@@ -0,0 +1,6 @@
+export interface IPerson {
+  name: string;
+  photo:null|string;
+  sex:number;
+  status: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { IPerson } from './Models/PersonInterface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
   title = 'LaraGular';
+  updateSatus(person: IPerson): void {
+    if (person.status === 1) {
+      alert("Désactivation du Status");
+    }else{
+      alert("Activation du Status");
+    }
+  }
+
+  persons: IPerson[] = [
+    {
+      name: "Yao yao Jules",
+      sex: 1,
+      status: 0,
+      photo: null
+    },
+    {
+      name: "Aïcha Diallo",
+      sex: 0,
+      status: 1,
+      photo: null
+    }
+
+  ]
+
+
 }
